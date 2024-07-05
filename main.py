5ed77e7313d1a08aea3f0c8aba1c99c0f951446e
Henry Hollmann <elfishhenry23@gmail.com>
2024-07-05 11:09:37 +0200

    Removed docker and gcloud because it was ass

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+import os
+import ezcord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+
+webhook_url = os.getenv("WEBHOOK_URL")
+
+# If the webhook URL is not set, prompt the user for input
+if not webhook_url:
+    webhook_url = input("Enter your webhook URL: ")
+    
+bot = ezcord.Bot(
+    intents=discord.Intents.default(),
+    #error_webhook_url = input("Error Webhook url: "),
+    error_webhook_url = os.getenv("ERROR_WEBHOOK_URL"),
+    language='auto',
+    default_language="en",
+)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name} ({bot.user.id})")
+    print("------")
+    print("Loading cogs...")
+    print("------")
+
+bot.load_cogs("cogs")
+bot.run(os.getenv("DISCORD_TOKEN"))  # Replace with your bot token

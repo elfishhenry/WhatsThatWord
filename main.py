ea088cc6a52075d2f7898aae379249b9c7933299
Henry Hollmann <109970614+elfishhenry@users.noreply.github.com>
2024-07-14 20:57:56 +0200

    feat: Implement /help command
    A new slash command, /help, is introduced to provide users with a comprehensive list of available commands and their descriptions. This enhances user experience by making it easier to discover and understand the bot's functionalities.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import ezcord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 
@@ -19,6 +20,33 @@ bot = ezcord.Bot(
     default_language="en",
 )
 
+@bot.slash_command(name="help", description="Lists all the commands and their descriptions.")
+async def help(ctx):
+    ctx.defer()
+
+    embed = discord.embed(title="Here's a list of all the commands:", colour=discord.Colour.gold())
+    embed.set_field(title="/Youtube", value="Search YouTube videos, returns a selected amount of results", inline=True)
+    embed.set_field(title="/Urban", value="Get the definition of a term(word) from Urban Dictionary.", inline=True)
+    embed.set_field(title="/Google", value="Google things!", inline=True)
+    embed.set_field(title="/Ai", value="Ask ai something", inline=True)
+    embed.set_field(title="User_command: Server Info", value="Get information about the server the command is used in.", inline=True)
+    embed.set_field(title="User_command: Account Creation Date", value="Get the date the account was created.", inline=True)
+    embed.set_field(title="Message_command: Get Message ID", value="Get the ID of the message the command is used in.", inline=True)
+    embed.set_field(title="/Tenor", value="Search for a GIF on Tenor", inline=True)
+    embed.set_field(title="/Image", value="Send an image as an attachment", inline=True)
+    embed.set_field(title="/Feedback", value="Send feedback to the bot developer", inline=True)
+    embed.set_field(title="date", value="Get the current date", inline=True)
+    embed.set_field(title="time", value="Get the current time", inline=True)
+    embed.set_field(title="datetime", value="Get the current date and time", inline=True)
+    embed.set_field(title="ai", description="Ask ai something", inline=True)
+
+    await ctx.send(embed=embed)
+
+
+
+
+
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name} ({bot.user.id})")
@@ -30,6 +58,7 @@ async def on_ready():
     print("Status changed!")
     print("------")
     print("Ready!")
+
 
 bot.load_cogs("cogs")
 bot.run(os.getenv("DISCORD_TOKEN"))  # Replace with your bot token
